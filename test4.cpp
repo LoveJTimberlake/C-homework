5753ab05e7b34b0ddb4550f356eb0921191992d5
Mozart <31980988+LoveJTimberlake@users.noreply.github.com>
2017-10-26 22:29:06 +0800

    Add files via upload

---
+++ test4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>  
+using namespace std;  
+ int main()  
+{  
+	int a, b, c;  
+	cout << "Please tell me three numbers and I will tell you whether they can form a triangle:" << endl;  
+	cin >> a >> b >> c;  
+	cout<<((a + b > c && a + c > b && b + c > a) ? char('Y') : char('N'));  
+	system("pause");  
+}  
