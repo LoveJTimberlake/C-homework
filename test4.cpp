215abe36614bebe0efc11b84e829e476f46d8c78
Mozart <31980988+LoveJTimberlake@users.noreply.github.com>
2017-10-27 22:06:36 +0800

    Add files via upload

--- test4.cpp
+++ test4.cpp
@@ -2,7 +2,7 @@
 using namespace std;  
  int main()  
 {  
-	int a, b, c;  
+	double a, b, c;  
 	cout << "Please tell me three numbers and I will tell you whether they can form a triangle:" << endl;  
 	cin >> a >> b >> c;  
 	cout<<((a + b > c && a + c > b && b + c > a) ? char('Y') : char('N'));  
